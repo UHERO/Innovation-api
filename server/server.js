42d6104d715b2fa8e2716daf1a51be96709d08b7
brandonbaris <brandonbaris@gmail.com>
2015-03-18 12:19:38 -1000

    Some small changes to data files

--- server/server.js
+++ server/server.js
@@ -13,6 +13,29 @@ app.get('/', function (req, res) {
   res.json({hello: "world"});
 });
 
+app.get('/economics/:name',function(req,res){
+  var options = {
+    root: __dirname + '/',
+    dotfiles: 'allow',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+   var fileName = req.params.name;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+  
+});
+
 // Server
 var server = app.listen(4567, function () {
   
